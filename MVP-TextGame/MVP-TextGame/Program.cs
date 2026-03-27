e58019f4844e64b57fff698d44abaf53ad110f91
Xelor-911 <caloun@spsejecna.cz>
2026-03-27 20:11:27 +0100

    Created console app in C#

---
+++ MVP-TextGame/MVP-TextGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MVP_TextGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
