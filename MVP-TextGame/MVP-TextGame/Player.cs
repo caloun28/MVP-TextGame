e7ed810c40652d0e655afa3790a8205e4ba89143
Xelor-911 <caloun@spsejecna.cz>
2026-04-06 22:08:57 +0200

    Created client program, player class and password hasher

--- MVP-TextGame/MVP-TextGame/Player.cs
+++ MVP-TextGame/MVP-TextGame/Player.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVP_TextGame
 {
-    internal class Player
+    public class Player
     {
+        public string Name { get; set; }
+        
+        public TcpClient Client { get; set; }
+        public StreamReader Reader { get; set; }
+        public StreamWriter Writer { get; set; }
+
+        public Player(string name, TcpClient client, StreamReader reader, StreamWriter writer)
+        {
+            Name = name;
+            Client = client;
+            Reader = reader;
+            Writer = writer;
+        }
     }
 }
